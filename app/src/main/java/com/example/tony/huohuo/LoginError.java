a295daf3ea0a94013ca2a635c9a42bf42ed2773e
Sherlobo <837021982@qq.com>
2017-02-25 11:40:33 +0800

    First commit.

---
+++ app/src/main/java/com/example/tony/huohuo/LoginError.java
@@ -0,0 +1,56 @@
+package com.example.tony.huohuo;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static java.security.AccessController.getContext;
+
+public class LoginError extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mCross;
+    private Button mUseIdentify;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_error);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar !=null){
+            actionBar.hide();
+        }
+        mCross =(Button) findViewById(R.id.Cross);
+        mCross.setOnClickListener(this);
+        mUseIdentify=(Button) findViewById(R.id.UseIdentify);
+        mUseIdentify.setOnClickListener(this);
+        String str="你可以通过手机号+短信验证码进行修改密码";
+        int fstart=str.indexOf("手机号+短信验证码");
+        int fend=fstart+"手机号+短信验证码".length();
+        SpannableStringBuilder style=new SpannableStringBuilder(str);
+        style.setSpan(new ForegroundColorSpan(Color.GREEN),fstart,fend, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        TextView mtext=(TextView) findViewById(R.id.text);
+        mtext.setText(style);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Cross:
+                finish();
+                break;
+            case R.id.UseIdentify:
+
+                break;
+            default:
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/tony/huohuo/register2Fragment.java
@@ -0,0 +1,19 @@
+package com.example.tony.huohuo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tony on 2017/2/14.
+ */
+
+public class register2Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.register2, container,false);
+    }
+}
