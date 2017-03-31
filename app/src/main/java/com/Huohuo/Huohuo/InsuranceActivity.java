b12ef36adc8c9252f3292fce3d48d7c88c2ff767
sherlobo <837021982@qq.com>
2017-03-31 19:24:51 +0800

    change

---
+++ app/src/main/java/com/Huohuo/Huohuo/InsuranceActivity.java
@@ -0,0 +1,32 @@
+package com.Huohuo.Huohuo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.Huohuo.Huohuo.base.BaseActivity;
+
+/**
+ * Created by JinBo on 2017/3/30.
+ */
+
+public class InsuranceActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insurance);
+        showContentView();
+        setTitle("保险理赔");
+        WebView webView=(WebView)findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://baidu.com");
+    }
+    public static void start(Context context) {
+        Intent intent = new Intent(context, InsuranceActivity.class);
+        context.startActivity(intent);
+    }
+
+}
--- app/src/main/java/com/Huohuo/Huohuo/home/child/HomeInsuranceFragment.java
+++ app/src/main/java/com/Huohuo/Huohuo/home/child/HomeInsuranceFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+import com.Huohuo.Huohuo.InsuranceActivity;
 import com.Huohuo.Huohuo.R;
 import com.Huohuo.Huohuo.adapter.InsuranceAdapter;
 import com.Huohuo.Huohuo.base.BaseFragment;
@@ -18,7 +20,7 @@ import java.util.List;
  * Created by yqhok on 2017-02-22.
  */
 
-public class HomeInsuranceFragment extends BaseFragment<FragmentHomeInsuranceBinding> {
+public class HomeInsuranceFragment extends BaseFragment<FragmentHomeInsuranceBinding>  implements View.OnClickListener{
     private List<Insurance> insuranceList=new ArrayList<>();
     private InsuranceAdapter adapter;
     @Override
@@ -27,6 +29,19 @@ public class HomeInsuranceFragment extends BaseFragment<FragmentHomeInsuranceBin
         showContentView();
         initInsurance();
         initRecycleView();
+        initView();
+    }
+
+    private void initView() {
+        bindingView.insurance.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.insurance:
+                InsuranceActivity.start(getActivity());
+                break;
+        }
     }
 
     private void initRecycleView() {
@@ -38,12 +53,10 @@ public class HomeInsuranceFragment extends BaseFragment<FragmentHomeInsuranceBin
     }
 
     private void initInsurance() {
-        for (int i = 0; i < 2; i ++) {
             Insurance insurance1 = new Insurance("普通货物运险","100元起");
             insuranceList.add(insurance1);
-            Insurance insurance2 = new Insurance("普通货物运险","100元起");
+            Insurance insurance2 = new Insurance("易碎货物运险","200元起");
             insuranceList.add(insurance2);
-        }
     }
 
     @Override
